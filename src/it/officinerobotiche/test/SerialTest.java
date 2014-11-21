b1200ed22a8ff84fc37434c3de2a7dbcdb45317e
Raffaello Bonghi <raffaello.bonghi@officinerobotiche.it>
2014-11-21 18:49:52 +0100

    Update test code

--- src/it/officinerobotiche/test/SerialTest.java
+++ src/it/officinerobotiche/test/SerialTest.java
@@ -5,11 +5,12 @@
  */
 package it.officinerobotiche.test;
 
-import it.officinerobotiche.message.Jmessage;
-import it.officinerobotiche.message.standard.Service;
-import it.officinerobotiche.message.SerialMessage;
-import it.officinerobotiche.serial.Packet;
+import it.officinerobotiche.message.*;
+import it.officinerobotiche.message.Jmessage.Command;
+import it.officinerobotiche.message.standard.*;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,29 +26,40 @@ public class SerialTest {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HELLO WORLD!");
-        
+
         //SerialBridge.availableSerialPorts();
-        
         ArrayList<Jmessage> list_send = new ArrayList<Jmessage>();
         ArrayList<Jmessage> list_receive;
-        
-        Service service = new Service(Service.Type.VERSION.getName());
-        list_send.add(service);
-        service = new Service(Service.Type.NAME_BOARD.getName());
-        list_send.add(service);
-        
-        
+
+        Service.NameService[] values = Service.NameService.values();
+
+//        list_send.add( new Service(Service.NameService.VERSION));
+//        list_send.add(new Service(Service.NameService.NAME_BOARD));
+//        list_send.add(new Service(Service.NameService.AUTHOR));
+//        list_send.add(new Service(Service.NameService.DATE_CODE));
+//        list_send.add(new MicroProcess(MicroProcess.TypeCommand.TIME));
         SerialMessage serial = new SerialMessage("/dev/tty.usbserial-A400g3iO");
         
-        try {
-            list_receive = serial.sendSyncMessage(list_send);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(SerialTest.class.getName()).log(Level.SEVERE, null, ex);
+//        list_receive = serial.sendSyncMessage(list_send);
+//        for (Jmessage message : list_receive) {
+//            try {
+//                Service mess = (Service) message;
+//                System.out.println(mess.getName());
+//            } catch (Exception ex) {
+//            }
+//        }
+//        list_send.clear();
+        
+        list_send.add(new MicroProcess(MicroProcess.TypeCommand.TIME));
+        list_receive = serial.sendSyncMessage(list_send);
+        for (Jmessage message : list_receive) {
+            try {
+                System.out.println(message.toString());
+            } catch (Exception ex) {
+            }
         }
         
-        Package p = SerialMessage.class.getPackage();
-        Class c = Service.class;
-        System.out.println("OK");
+        serial.close();
     }
-    
+
 }
