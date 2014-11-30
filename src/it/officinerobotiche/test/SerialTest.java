cb1d5d9241f854e323ab223e5b1a8bf4aeff5a53
Raffaello Bonghi <raffaello.bonghi@officinerobotiche.it>
2014-11-30 20:00:29 +0100

    Create a First Example to encodes and decodes a Frames

--- src/it/officinerobotiche/test/SerialTest.java
+++ src/it/officinerobotiche/test/SerialTest.java
@@ -5,10 +5,17 @@
  */
 package it.officinerobotiche.test;
 
-import it.officinerobotiche.message.*;
-import it.officinerobotiche.message.Jmessage.Command;
-import it.officinerobotiche.message.standard.*;
-import java.lang.reflect.Field;
+import it.officinerobotiche.serial.frame.unav.UnavFrame;
+import it.officinerobotiche.serial.frame.unav.PID;
+import it.officinerobotiche.serial.frame.standard.Service;
+import it.officinerobotiche.serial.frame.standard.StandardFrame;
+import it.officinerobotiche.serial.frame.standard.MProcess;
+import it.officinerobotiche.serial.frame.SerialFrame;
+import it.officinerobotiche.serial.frame.AbstractFrame;
+import it.officinerobotiche.serial.frame.FrameEvent;
+import it.officinerobotiche.serial.frame.ParserListener;
+import it.officinerobotiche.serial.frame.standard.ErrorSerial;
+import it.officinerobotiche.serial.frame.standard.MProcess.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -16,50 +23,88 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author raffaello
+ * @author Raffaello
  */
-public class SerialTest {
+public class SerialTest implements ParserListener {
+
+    @Override
+    public void frameEvent(FrameEvent evt) {
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("HELLO WORLD!");
 
-        //SerialBridge.availableSerialPorts();
-        ArrayList<Jmessage> list_send = new ArrayList<Jmessage>();
-        ArrayList<Jmessage> list_receive;
+        //SerialMessage.availableSerialPorts();
+        String name = "/dev/tty.usbserial-A400g3iO";
+        SerialFrame port = new SerialFrame(name);
 
-        Service.NameService[] values = Service.NameService.values();
+        ArrayList<AbstractFrame> list = new ArrayList<>();
+        Service service = new Service(Service.NameService.VERSION);
+        list.add(service);
+        //list.add(new Service(Service.NameService.AUTHOR));
+        list.add(new MProcess.Time());
+        ErrorSerial err = new ErrorSerial();
 
-//        list_send.add( new Service(Service.NameService.VERSION));
-//        list_send.add(new Service(Service.NameService.NAME_BOARD));
-//        list_send.add(new Service(Service.NameService.AUTHOR));
-//        list_send.add(new Service(Service.NameService.DATE_CODE));
-//        list_send.add(new MicroProcess(MicroProcess.TypeCommand.TIME));
-        SerialMessage serial = new SerialMessage("/dev/tty.usbserial-A400g3iO");
-        
-//        list_receive = serial.sendSyncMessage(list_send);
-//        for (Jmessage message : list_receive) {
-//            try {
-//                Service mess = (Service) message;
-//                System.out.println(mess.getName());
-//            } catch (Exception ex) {
-//            }
-//        }
-//        list_send.clear();
-        
-        list_send.add(new MicroProcess(MicroProcess.TypeCommand.TIME));
-        list_receive = serial.sendSyncMessage(list_send);
-        for (Jmessage message : list_receive) {
-            try {
-                System.out.println(message.toString());
-            } catch (Exception ex) {
+        try {
+            List<AbstractFrame> sendSyncMessage = port.sendSyncFrame(list);
+
+            for (AbstractFrame m : sendSyncMessage) {
+                decode(m);
             }
+
+            Service sendSyncMessage1 = port.sendSyncFrame(new Service(Service.NameService.VERSION));
+            decode(sendSyncMessage1);
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SerialTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        serial.close();
+
+        port.close();
     }
 
+    public static void decode(AbstractFrame m) {
+        switch (m.getTypeMessage()) {
+            case DEFAULT:
+                defaultMessage((StandardFrame) m);
+                break;
+            case UNAV:
+                unavMessage((UnavFrame) m);
+                break;
+        }
+    }
+
+    public static void unavMessage(UnavFrame message) {
+        switch (message.getCommand()) {
+            case PID_L:
+                PID.PIDLeft mm = (PID.PIDLeft) message;
+                break;
+            case PID_R:
+                PID.PIDRight mm2 = (PID.PIDRight) message;
+                break;
+        }
+    }
+
+    public static void defaultMessage(StandardFrame message) {
+        switch (message.getCommand()) {
+            case TIME_PROCESS:
+                MProcess.Time time = (MProcess.Time) message;
+                int[] processes = time.getProcesses();
+                System.out.println(processes.toString());
+                break;
+            case FRQ_PROCESS:
+//                MProcess.Frequency mm = (MProcess.Frequency) message;
+//                mm.getFrequency();
+                break;
+            case PRIORITY_PROCESS:
+//                MProcess.Priority mm2 = (MProcess.Priority) message;
+                break;
+            case SERVICES:
+                Service s = (Service) message;
+                System.out.println(s.getInformation());
+                System.out.println(s.isSync());
+                break;
+        }
+    }
 }
